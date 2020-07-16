93095dbb69b3e7d7b0ae00025c9b5e805e6d35f7
Pierpaolo Polo <ancheno@gmail.com>
2020-07-16 12:42:22 +0200

    Initial commit

---
+++ lib/melkdev_view_tool.rb
@@ -0,0 +1,7 @@
+require "melkdev_view_tool/version"
+require "melkdev_view_tool/renderer"
+
+module MelkdevViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/melkdev_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MelkdevViewTool
+  VERSION = "0.1.0"
+end
